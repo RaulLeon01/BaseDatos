322e32d4d7f765f8e18991a2fd172fb703317c38
RaulLeon01 <20243rd013@utez.edu.mx>
2025-07-08 12:04:33 -0600

    Creación de la variable wallet
    Aqui estará alojada la cartera

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,18 @@
 package org.example;
-
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        // Variables
+        String wallet = "G:\\1-UTEZ\\3-.Cuatrimestre\\POO\\Trabajos\\Semana11\\BaseDatos\\BaseDatos\\src\\Wallet";
+        System.setProperty("oracle.net.tns_admin", wallet);
+
+        //String jdbcUrl = "jdbc:oracle:thin:@";
+
         //
+
     }
 }
